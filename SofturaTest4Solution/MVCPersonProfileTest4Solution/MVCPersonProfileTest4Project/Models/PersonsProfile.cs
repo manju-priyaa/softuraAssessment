8191f7a74fad8847480734b9e79a7566b9310475
Manju Priyaa R <mailmanjupriyaa@gmail.com>
2021-05-21 21:14:27 +0530

    Changes -Manju

---
+++ SofturaTest4Solution/MVCPersonProfileTest4Solution/MVCPersonProfileTest4Project/Models/PersonsProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPersonProfileTest4Project.Models
+{
+    public class PersonsProfile
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Qualification { get; set; }
+        public string IsEmployed{ get; set; }
+        public string NoticePeriod { get; set; }
+        public float CurrentCTC { get; set; }
+
+    }
+}
---
+++ SofturaTest4Solution/SQLQuery.sql
@@ -0,0 +1,8 @@
+--use dbPersonProfile
+--select * from personsprofiles;
+
+
+use dbWeather;
+select * from Weathers
+
+insert into Weathers(City,Date,LowTemp,HighTemp,Forecast) values('Chennai','2017-06-21 12:00:50','25 Degree','36 Degree','cloudy');
